400fd692048b8d8d4a72c478e315d779c2dc6112
raba72010 <44120054+raba72010@users.noreply.github.com>
2026-08-19 22:39:39 +0300

    Finalize BeforeWise data bundle

---
+++ beforewise/data-final.js
@@ -0,0 +1,2 @@
+const CATEGORIES = ['All','Travel','Work','Home & Life','Buying & Money','Digital','Saudi & GCC','Events & Social','Family'];
+window.BEFOREWISE_DATA = {SCENARIOS,CATEGORIES,SOURCE_LIBRARY};
